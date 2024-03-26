b45840fcc844e889311dc42e64effd9d421fcb2e
Woola Johnson <woolajohnson@gmail.com>
2024-03-26 18:19:44 +0800

    Done React Simple List

--- simple_list/src/App.jsx
+++ simple_list/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import saitamaImage from "./assets/images/saitama.jpg";
+import genosImage from "./assets/images/genos.jpg";
+import blastImage from "./assets/images/blast.jpg";
+import "./assets/stylesheets/Style.css";
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <>
+//                 <ul>
+//                     <li>
+//                         <img src={saitamaImage} alt="Image of Saitama" />
+//                         <section>
+//                             <div>
+//                                 <h2>Saitama</h2>
+//                                 <p>March 26, 2024 3:00 PM</p>
+//                             </div>
+//                             <h3>Caped Baldy</h3>
+//                         </section>
+//                     </li>
+//                     <li>
+//                         <img src={genosImage} alt="Image of Genos" />
+//                         <section>
+//                             <div>
+//                                 <h2>Genos</h2>
+//                                 <p>March 26, 2024 5:22 PM</p>
+//                             </div>
+//                             <h3>Demon Cyborg</h3>
+//                         </section>
+//                     </li>
+//                     <li>
+//                         <img src={blastImage} alt="Image of Blast" />
+//                         <section>
+//                             <div>
+//                                 <h2>Blast</h2>
+//                                 <p>March 26, 2024 7:35 PM</p>
+//                             </div>
+//                             <h3>Rank 1 Hero</h3>
+//                         </section>
+//                     </li>
+//                 </ul>
+//             </>
+//         );
+//     }
+// }
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <ul>
+                <li>
+                    <img src={saitamaImage} alt="Image of Saitama" />
+                    <section>
+                        <div>
+                            <h2>Saitama</h2>
+                            <p>March 26, 2024 3:00 PM</p>
+                        </div>
+                        <h3>Caped Baldy</h3>
+                    </section>
+                </li>
+                <li>
+                    <img src={genosImage} alt="Image of Genos" />
+                    <section>
+                        <div>
+                            <h2>Genos</h2>
+                            <p>March 26, 2024 5:22 PM</p>
+                        </div>
+                        <h3>Demon Cyborg</h3>
+                    </section>
+                </li>
+                <li>
+                    <img src={blastImage} alt="Image of Blast" />
+                    <section>
+                        <div>
+                            <h2>Blast</h2>
+                            <p>March 26, 2024 7:35 PM</p>
+                        </div>
+                        <h3>Rank 1 Hero</h3>
+                    </section>
+                </li>
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
